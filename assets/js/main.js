16e93721bda2d333a872d7b0fa58caa8ce695737
Bill Dean <742designs@gmail.com>
2016-09-17 16:28:33 -0400

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,40 @@
+var unit = document.querySelector('input[name="unit"]:checked').value;
+var unitIcon = "°C";
+if (unit = "imperial") {
+  unitIcon = "°F";
+}
+console.log("Units: " + unit + unitIcon);
+
+function getCity(){
+  var city = document.getElementById('cityBox').value;
+
+  // Ajax request data
+  var wapi = "http://api.openweathermap.org/data/2.5/forecast/weather?q=" + city + "&units=" + unit + "&id=524901&APPID=b32c84201db94b92ed42d393cac6526b";
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', wapi, false);
+  xhr.send(null);
+
+  console.log(wapi);
+
+  var output;
+
+  // check for connection
+  if (xhr.response = 200) {
+
+    var data = JSON.parse(xhr.responseText);
+    //console.log(data);
+
+    var temp = data.list[0].main.temp;
+    var splitTemp = JSON.stringify(temp).split('.', 1);
+
+    output = '<p>City: ' + data.city.name + '</p>';
+    output += '<img src=http://openweathermap.org/img/w/' + data.list[0].weather[0].icon + '.png>';
+    output += data.list[0].weather[0].main;
+    output += '<p>Current Temperature: ' + splitTemp + unitIcon + '</p>';
+
+    document.getElementById('dataBox').innerHTML = output;
+  } else {
+    output = "Error - no response";
+  }
+
+}
---
+++ gulpfile.js
@@ -0,0 +1,67 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync'),
+    concat = require('gulp-concat');
+    uglify = require('gulp-uglify');
+
+var cssSources,
+    jsSources;
+
+cssSources = [
+  'assets/css/*.sass'
+];
+
+jsSources = [
+  'assets/js/*.js'
+];
+
+gulp.task('sass', function(){
+  return gulp.src(cssSources)
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }))
+    .pipe(gulp.dest('css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
+gulp.task('js', function() {
+  gulp.src(jsSources)
+      .pipe(concat('scripts.js'))
+      .pipe(uglify())
+      .pipe(gulp.dest('js'))
+      .pipe(browserSync.reload({
+        stream: true
+      }));
+});
+
+gulp.task('html', function(){
+	gulp.src('*.html')
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
+gulp.task('misc', function(){
+	gulp.src(['js/*.json'])
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
+gulp.task('watch', ['browserSync', 'js', 'misc', 'sass'], function(){
+  gulp.watch(cssSources, ['sass']);
+  gulp.watch('*.html', ['html']);
+  gulp.watch(jsSources, ['js']);
+});
+
+gulp.task('browserSync', function() {
+  browserSync({
+    server: {
+      baseDir: "./"
+    },
+  });
+});
+
+gulp.task('default', ['sass', 'js', 'html', 'misc', 'watch']);
