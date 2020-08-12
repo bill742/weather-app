5dcc43ece2f1b88e13ee6b0db346eb8bb034f44d
Bill Dean <742designs@gmail.com>
2020-08-11 23:10:54 -0400

    Fixing linting errors

--- assets/js/data.js
+++ assets/js/data.js
@@ -40,38 +40,38 @@
         // TODO: fetch data with async/await?
         setTimeout(() => {
             fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=${unit}&id=524901&APPID=b32c84201db94b92ed42d393cac6526b`)
-            .then(response => response.json())
-            .then(json => {
-                console.log(json);
-                let splitTemp = JSON.stringify(json.current.temp).split('.', 1);
-                let splitFeel = JSON.stringify(json.current.feels_like).split('.', 1);
-                // output += `<p class="data-city">${city}</p>`;
-                output += `<i class="owf owf-${json.current.weather[0].id}"></i>`;
-                output += `<p class="unit-text">${json.current.weather[0].main}</p>`
-                output += `<p id="unitText" class="unit-text">Current Temperature: ${splitTemp} ${unitIcon}</p>`
-                output += `<p class="unit-text">Feels like: ${splitFeel} ${unitIcon}</p>`
-                dataBox.innerHTML = output;
+                .then(response => response.json())
+                .then(json => {
+                    console.log(json);
+                    let splitTemp = JSON.stringify(json.current.temp).split('.', 1);
+                    let splitFeel = JSON.stringify(json.current.feels_like).split('.', 1);
+                    // output += `<p class="data-city">${city}</p>`;
+                    output += `<i class="owf owf-${json.current.weather[0].id}"></i>`;
+                    output += `<p class="unit-text">${json.current.weather[0].main}</p>`;
+                    output += `<p id="unitText" class="unit-text">Current Temperature: ${splitTemp} ${unitIcon}</p>`;
+                    output += `<p class="unit-text">Feels like: ${splitFeel} ${unitIcon}</p>`;
+                    dataBox.innerHTML = output;
 
-                let code = json.current.weather[0].id;
-                getImages(code);
-            }
-            );
+                    let code = json.current.weather[0].id;
+                    getImages(code);
+                }
+                );
         }, 1000);
 
         unitBtn.classList.add('visible');
         unitBtn.addEventListener('click', () => { changeUnit(isCelcius); }, false);
-    };
+    }
 
     function geoSuccess(pos) {
         var crd = pos.coords;
         var lat = `${crd.latitude}`;
         var lon = `${crd.longitude}`;
-        getData(lat, lon, isCelcius)
+        getData(lat, lon, isCelcius);
     }
 
     function geoError(err) {
         console.warn(`ERROR(${err.code}): ${err.message}`);
-        document.getElementById('dataBox').innerHTML = '<p>Location not available. Please search for a location.</p>'
+        document.getElementById('dataBox').innerHTML = '<p>Location not available. Please search for a location.</p>';
     }
 
     function changeUnit(isCelcius) {
