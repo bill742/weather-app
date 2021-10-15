b44a2c2a0b6495a98ffc4c3067ec3b888687bea9
Bill Dean <742designs@gmail.com>
2021-10-14 23:24:00 -0300

    Adjust spacing.

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,7 @@ const Header = () => {
     const template = `
         <header>
             <h1>Weather App</h1>
-            <h2>
-                See your weather conditions or search for the forecast in cities
-                around the world
+            <h2>See your weather conditions or search for the forecast in cities around the world
             </h2>
         </header>
     `;
