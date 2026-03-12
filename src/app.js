6466986c40eba60548aee2eff8e3d25c7c5753f1
Bill Dean <742designs@gmail.com>
2026-03-12 18:05:47 -0400

    Remove Tailwind as it is not being utilized very much.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import Header from './components/Header';
 import WeatherWidget from './components/Data';
 import Search from './components/Search';
-import './css/tailwind.css';
 import './css/owfont-regular.min.css';
 import './sass/app.sass';
 
